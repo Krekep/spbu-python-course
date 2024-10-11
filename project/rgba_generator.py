ab092d67e6d2c9a36a18ba509315cc29ce0788a3
GusevaVarvara <st119025@student.spbu.ru>
2024-10-11 23:47:41 +0300

    Реализован генератор для четырехмерного набора векторов RGBA и функция, возвращающая i элемент этого набора.

---
+++ project/rgba_generator.py
@@ -0,0 +1,20 @@
+import itertools
+
+
+def rgba_generator():
+    for r in range(256):
+        for g in range(256):
+            for b in range(256):
+                for a in range(0, 101, 2):
+                    yield r, g, b, a
+
+
+def get_rgba_element(i):
+    if i <= 0:
+        return "Error: index must be greater than 0. The numbering of elements in a set of vectors starts from 1."
+    max_elements = 256 * 256 * 256 * 51
+    if i > max_elements:
+        return "Error: index must be within the number of possible vectors."
+    rgba_gen = rgba_generator()
+    element = next(itertools.islice(rgba_gen, i - 1, i), None)
+    return element if element else "Error: index out of range."
---
+++ tests/test_rgba_generator.py
@@ -0,0 +1,48 @@
+import pytest
+from project.rgba_generator import rgba_generator, get_rgba_element
+
+
+def test_rgba_generator():
+    gen = rgba_generator()
+    assert next(gen) == (0, 0, 0, 0)
+    assert next(gen) == (0, 0, 0, 2)
+    assert next(gen) == (0, 0, 0, 4)
+    assert next(gen) == (0, 0, 0, 6)
+    for _ in range(51 - 4):
+        next(gen)
+    assert next(gen) == (0, 0, 1, 0)
+
+
+@pytest.mark.parametrize(
+    "i, expected",
+    [
+        (1, (0, 0, 0, 0)),
+        (10, (0, 0, 0, 18)),
+        (51 + 1, (0, 0, 1, 0)),
+        (51 * 256 + 1, (0, 1, 0, 0)),
+        (51 * 256 * 256 + 1, (1, 0, 0, 0)),
+    ],
+)
+def test_get_rgba_element(i, expected):
+    assert get_rgba_element(i) == expected
+
+
+@pytest.mark.parametrize(
+    "i, expected_error",
+    [
+        (
+            -1,
+            "Error: index must be greater than 0. The numbering of elements in a set of vectors starts from 1.",
+        ),
+        (
+            0,
+            "Error: index must be greater than 0. The numbering of elements in a set of vectors starts from 1.",
+        ),
+        (
+            256**3 * 51 + 1,
+            "Error: index must be within the number of possible vectors.",
+        ),
+    ],
+)
+def test_get_rgba_element_errors(i, expected_error):
+    assert get_rgba_element(i) == expected_error
