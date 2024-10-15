39225ff9f55ed2e411f3dd22897903354e0f4513
AngelinaYakubovskaya <lina7305@mail.ru>
2024-10-15 21:39:42 +0300

    Refactor test_curry_uncarry.py to use pytest instead of unittest

--- tests/test_curry_uncarry.py
+++ tests/test_curry_uncarry.py
@@ -1,36 +1,15 @@
-import unittest
+import pytest
+from project.curry_uncarry import curry_explicit, uncurry_explicit
 
 
-def curry_explicit(function, arity):
-    def curry(*args):
-        if len(args) == arity:
-            return function(*args)
-
-        def curried(*more_args):
-            return curry(*(args + more_args))
-
-        return curried
-
-    return curry
+def test_curry_function():
+    """Test the curried function."""
+    curried_add = curry_explicit(lambda x, y: x + y, 2)
+    assert curried_add(3)(4) == 7  # Test the curried function
 
 
-def uncurry_explicit(function, arity):
-    def uncurried(*args):
-        return function(*args)
-
-    return uncurried
-
-
-class TestCurryUncurryFunctions(unittest.TestCase):
-    def test_curry_function(self):
-        curried_add = curry_explicit(lambda x, y: x + y, 2)
-        self.assertEqual(curried_add(3)(4), 7)  # Test the curried function
-
-    def test_uncurry_function(self):
-        curried_add = curry_explicit(lambda x, y: x + y, 2)
-        uncurried_add = uncurry_explicit(curried_add, 2)
-        self.assertEqual(uncurried_add(5, 7), 12)  # Test the uncurried function
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_uncurry_function():
+    """Test the uncurried function."""
+    curried_add = curry_explicit(lambda x, y: x + y, 2)
+    uncurried_add = uncurry_explicit(curried_add, 2)
+    assert uncurried_add(5, 7) == 12  # Test the uncurried function
