47ba40490a57f8583ac7a32a7f8654ca3344c98a
GusevaVarvara <st119025@student.spbu.ru>
2024-10-23 12:27:38 +0300

    Исправлена документация.

--- project/thread_pool.py
+++ project/thread_pool.py
@@ -7,8 +7,7 @@ class ThreadPool:
     The pool creates a specified number of threads that wait for tasks to be added to the task queue.
     When a task is added, one of the threads retrieves and executes it.
 
-    :param num_threads: The number of threads to create in the pool. This dictates how many tasks
-                        can be executed simultaneously.
+    :param num_threads: The number of threads to create in the pool. This dictates how many tasks can be executed simultaneously.
     """
 
     def __init__(self, num_threads: int):
@@ -16,6 +15,7 @@ class ThreadPool:
         Initializes the thread pool with the specified number of threads.
 
         :param num_threads: The number of threads to create in the pool.
+        :return: None
         """
         self.num_threads: int = num_threads
         self.tasks: List[Callable] = []
@@ -30,6 +30,7 @@ class ThreadPool:
         Creates and starts threads that will execute tasks from the pool.
         The threads are set as daemon threads, meaning they will automatically exit
         when the main program exits.
+        :return: None
         """
         for _ in range(self.num_threads):
             thread = threading.Thread(target=self._worker, daemon=True)
@@ -40,6 +41,7 @@ class ThreadPool:
         """
         The worker method that runs in each thread.
         It continuously waits for new tasks and executes them until the pool is disposed.
+        :return: None
         """
         while not self.stopped.is_set():
             with self.task_available:
@@ -61,6 +63,7 @@ class ThreadPool:
         :param task: A callable object (function) to be executed by one of the threads.
         :raises RuntimeError: If the pool has been disposed and new tasks are not allowed.
         :raises ValueError: If the task is not callable.
+        :return: None
         """
         if self.stopped.is_set():
             raise RuntimeError("Cannot add tasks after the pool is disposed.")
@@ -74,6 +77,7 @@ class ThreadPool:
         """
         Terminates the thread pool, notifying all threads to finish their
         current tasks and stop. Blocks until all threads have completed their tasks.
+        :return: None
         """
         self.stopped.set()
         with self.task_available:
