c8b07062d1efe59cce39b76ac40e990d82a9259c
Kirill-ML19 <kirillvoronkov896@gmail.com>
2024-11-07 20:09:04 +0300

    perf(cartesian_tree): created a class responsible for the implementation of the tree

--- project/Cartesian_tree/cartesian_tree.py
+++ project/Cartesian_tree/cartesian_tree.py
@@ -19,3 +19,52 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+
+
+class Treap(MutableMapping):
+    """
+    The 'Treap' class implements a Cartesian tree,
+    which is a self-balancing binary search tree.
+
+    This tree uses node priorities to maintain balance.
+    Priorities are randomly generated.
+
+    Tracks can be used to implement dictionaries, sets,
+    as well as for other tasks
+    where efficient sorting and searching are needed.
+
+     Attributes:
+            root (Node): The root node of the tree.
+    """
+
+    def __init__(self):
+        self.root = None
+
+    def __len__(self):
+        """
+        returns the number of nodes in the tree
+        """
+        return self._size(self.root)
+
+    def _size(self, node):
+        """
+        a function for counting nodes in a subtree
+        """
+        if node is None:
+            return 0
+        return 1 + self._size(node.left) + self._size(node.right)
+
+    def __contains__(self, key):
+        """
+        checks if there is a node with the specified key
+        """
+        return self.find(key) is not None
+
+    def __getitem__(self, key):
+        """
+        The function returns the value associated with the specified key
+        """
+        node = self.find(key)
+        if node is None:
+            raise KeyError(key)
+        return node.value
