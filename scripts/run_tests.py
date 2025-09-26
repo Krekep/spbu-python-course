e63be83d9ef062051bfdef5b414a075d79066b76
Якубовская Анастасия <jakubovkaja@yandex.ru>
2025-09-27 00:23:22 +0300

    rename tests

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,9 +1,13 @@
 import subprocess
+import os
+import sys
 import shared
 
 
 def main():
-    shared.configure_python_path()
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, project_root)
+
     subprocess.check_call(["python", "-m", "pytest", "-vv", "-s", shared.TESTS])
 
 
