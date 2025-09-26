aec5b77349d7bcb24dfff341777056ace30a3405
Rodionova Ksenia <st132496@student.spbu.ru>
2025-09-26 19:09:18 +0300

    Update tests_algebra.py

--- homeworks/tests_algebra.py
+++ homeworks/tests_algebra.py
@@ -1,6 +1,6 @@
-import putest
+import pytest
 import numpy as np
-from algebra_operations import*
+from algebra import*
 
 class TestAlgebra:
   @pytest.mark.parametrize('a,b,expected', [
@@ -11,18 +11,18 @@ class TestAlgebra:
 
   def test_Scalar(self, a, b, expected): #скалярное произведение
     result = Scalar(a, b)
-    assert result == expected
+    np.testing.assert_array_equal(result, expected)
 
   def test_Normal(self):
     a = np.array([0, 1]) #длина вектора
-    expected = 2
+    expected = 5.0
     result = Normal(a)
-    assert result == expected
+    np.testing.assert_array_equal(result, expected)
 
   def test_Angle(self):
     a = np.array([1, 0]) # 90 градусов
     b = np.array([0, 1])
-    assert Angle(a, b) == ytest.approx(90.0, abs = 1e-5)
+    assert Angle(a, b) == pytest.approx(90.0, abs = 1e-5)
 
     a = np.array([1, 2, 3]) # 0 градусов
     b = np.array([2, 4, 6])
@@ -32,18 +32,18 @@ class TestAlgebra:
     M = np.array([[1, 2], [3,4]])
     expected = np.array([[1,3],[2,4]])
     result = Trans(M)
-    assert result == expected
-
+    np.testing.assert_array_equal(result, expected)
+    
   def test_Multiplication(self): #умножение матриц
     A = np.array([[1, 2], [3, 4]])
     B = np.array([[5, 6], [7, 8]])
     expected = np.array([[19, 22], [43, 50]])
     result = Multiplication(A, B)
-    assert result == expected
+    np.testing.assert_array_equal(result, expected)
 
   def test_Summa(self): #сумма матриц
     A = np.array([[1, 2], [3, 4]])
     B = np.array([[5, 6], [7, 8]])
     expected = A + B
     result = Summa(A, B)
-    assert result == expected
+    np.testing.assert_array_equal(result, expected)
