81a2e39937398f48caecdfcc75a738f97fda8b14
Якубовская Анастасия <jakubovkaja@yandex.ru>
2025-09-28 01:48:37 +0300

    change run_tests.py

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,14 +1,15 @@
 import subprocess
-import os
 import sys
-import shared
+import os
 
 
 def main():
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.insert(0, project_root)
 
-    subprocess.check_call(["python", "-m", "pytest", "-vv", "-s", shared.TESTS])
+    tests_dir = os.path.join(project_root, "tests")
+
+    subprocess.check_call([sys.executable, "-m", "pytest", "-vv", "-s", tests_dir])
 
 
 if __name__ == "__main__":
